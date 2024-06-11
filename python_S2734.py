b306c51f8bd2bfda6941dbfb35f1b117c31d4f97
pmj612 <163090159+pmj612@users.noreply.github.com>
2024-06-11 14:44:11 +0900

    Update python_S2734.py

--- python_S2734.py
+++ python_S2734.py
@@ -2,3 +2,6 @@ class MyClass(object):
     def __init__(self):
         self.message = 'Hello'
         return self  # Noncompliant: a TypeError will be raised.
+
+def generate():
+        numbers = random.sample(range(1, 47), 6)
